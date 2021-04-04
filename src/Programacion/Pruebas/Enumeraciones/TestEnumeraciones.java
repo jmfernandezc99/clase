5ca7b9665d6de142bd3dcf5b93ba926df796fe79
Juanma <jmfernandezc99@gmail.com>
2021-04-04 15:45:48 +0200

    Ejercicio practica enumeraciones simples y con argumentos

---
+++ src/Programacion/Pruebas/Enumeraciones/TestEnumeraciones.java
@@ -0,0 +1,8 @@
+package Programacion.Pruebas.Enumeraciones;
+
+public class TestEnumeraciones {
+    public static void main(String[] args) {
+        System.out.println("Continente Nº 4: " + Continentes.AMERICA);
+        System.out.println("Países en el 4º continente: " + Continentes.AMERICA.getPaises());
+    }
+}
