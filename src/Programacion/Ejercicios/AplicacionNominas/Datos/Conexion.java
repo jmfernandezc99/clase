01e2d5cdb62b41ffbe1d3a1572584974f2135dab
Juanma <jmfernandezc99@gmail.com>
2021-04-27 16:46:54 +0200

    Creación del archivo de conexión a la base de datos.

---
+++ src/Programacion/Ejercicios/AplicacionNominas/Datos/Conexion.java
@@ -0,0 +1,34 @@
+package Programacion.Ejercicios.AplicacionNominas.Datos;
+
+import java.sql.*;
+
+public class Conexion {
+    private static final String JDBC_URL = "jdbc:MySQL://localhost:3306/Nominas";
+    private static final String JDBC_USER = "juanma";
+    private static final String JDBC_PASSWORD = JDBC_USER;
+
+    //Metodo para obtener la conexion con la base de datos
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+
+    //Metodo para cerrar el ResultSet
+    public static void close(ResultSet rs) throws SQLException {
+        rs.close();
+    }
+
+    //Metodo para cerrar el Statement
+    public static void close(Statement smtm) throws SQLException {
+        smtm.close();
+    }
+
+    //Metodo para cerrar el PreparedStatement
+    public static void close(PreparedStatement smtm) throws SQLException {
+        smtm.close();
+    }
+
+    //Metodo para cerrar la conexion
+    public static void close(Connection conn) throws SQLException {
+        conn.close();
+    }
+}
